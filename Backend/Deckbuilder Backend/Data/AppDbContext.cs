a2808f0253f50dac6a8e634f1dfcd2edd1255685
Exocore-Mashina <jonas.kaistra@gmail.com>
2025-12-15 14:14:54 +0100

    Backend and Database setup

---
+++ Backend/Deckbuilder Backend/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Deckbuilder_Backend.Models;
+
+namespace Deckbuilder_Backend.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Card> Cards => Set<Card>();
+}
---
+++ Backend/Deckbuilder Backend/Models/Card.cs
@@ -0,0 +1,10 @@
+﻿namespace Deckbuilder_Backend.Models;
+
+public class Card
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public int Cost { get; set; }
+}
